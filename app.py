74357e2d6efae76882eac5ba8ab5f33c5549af75
Ahmad Zia <ah.ziayosfi@gmail.com>
2019-09-14 04:14:04 +0430

    problem finded
    problem was with the traing line in traing.py

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 from trainig import AttnDecoderRNN
+from trainig import EncoderRNN
+from trainig import Lang
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+##device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 app = Flask(__name__)
 #model = pickle.load(open('model.pkl', 'rb'))
@@ -28,46 +30,46 @@ def home():
 
 #importing 
 
-MAX_LENGTH = 10
+#MAX_LENGTH = 10
 
 #the Decoder and encoder classes
 
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
+# class DecoderRNN(nn.Module):
+#     def __init__(self, hidden_size, output_size):
+#         super(DecoderRNN, self).__init__()
+#         self.hidden_size = hidden_size
 
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+#         self.embedding = nn.Embedding(output_size, hidden_size)
+#         self.gru = nn.GRU(hidden_size, hidden_size)
+#         self.out = nn.Linear(hidden_size, output_size)
+#         self.softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
-        return output, hidden
+#     def forward(self, input, hidden):
+#         output = self.embedding(input).view(1, 1, -1)
+#         output = F.relu(output)
+#         output, hidden = self.gru(output, hidden)
+#         output = self.softmax(self.out(output[0]))
+#         return output, hidden
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+#     def initHidden(self):
+#         return torch.zeros(1, 1, self.hidden_size, device=device)
 
-class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(EncoderRNN, self).__init__()
-        self.hidden_size = hidden_size
+# class EncoderRNN(nn.Module):
+#     def __init__(self, input_size, hidden_size):
+#         super(EncoderRNN, self).__init__()
+#         self.hidden_size = hidden_size
 
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+#         self.embedding = nn.Embedding(input_size, hidden_size)
+#         self.gru = nn.GRU(hidden_size, hidden_size)
 
-    def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden)
-        return output, hidden
+#     def forward(self, input, hidden):
+#         embedded = self.embedding(input).view(1, 1, -1)
+#         output = embedded
+#         output, hidden = self.gru(output, hidden)
+#         return output, hidden
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+#     def initHidden(self):
+#         return torch.zeros(1, 1, self.hidden_size, device=device)
 
 
 # class AttnDecoderRNN(nn.Module):
@@ -85,26 +87,26 @@ class EncoderRNN(nn.Module):
 #         self.gru = nn.GRU(self.hidden_size, self.hidden_size)
 #         self.out = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self, input, hidden, encoder_outputs):
-        embedded = self.embedding(input).view(1, 1, -1)
-        embedded = self.dropout(embedded)
+    # def forward(self, input, hidden, encoder_outputs):
+    #     embedded = self.embedding(input).view(1, 1, -1)
+    #     embedded = self.dropout(embedded)
 
-        attn_weights = F.softmax(
-            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
-        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
-                                 encoder_outputs.unsqueeze(0))
+    #     attn_weights = F.softmax(
+    #         self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+    #     attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+    #                              encoder_outputs.unsqueeze(0))
 
-        output = torch.cat((embedded[0], attn_applied[0]), 1)
-        output = self.attn_combine(output).unsqueeze(0)
+    #     output = torch.cat((embedded[0], attn_applied[0]), 1)
+    #     output = self.attn_combine(output).unsqueeze(0)
 
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
+    #     output = F.relu(output)
+    #     output, hidden = self.gru(output, hidden)
 
-        output = F.log_softmax(self.out(output[0]), dim=1)
-        return output, hidden, attn_weights
+    #     output = F.log_softmax(self.out(output[0]), dim=1)
+    #     return output, hidden, attn_weights
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+    # def initHidden(self):
+    #     return torch.zeros(1, 1, self.hidden_size, device=device)
 
 
 # Loading the modeles
@@ -116,128 +118,128 @@ encoder=torch.load("encoder1")
 SOS_token = 0
 EOS_token = 1
 
-class Lang:
-    def __init__(self, name):
-        self.name = name
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {0: "SOS", 1: "EOS"}
-        self.n_words = 2  # Count SOS and EOS
+# class Lang:
+#     def __init__(self, name):
+#         self.name = name
+#         self.word2index = {}
+#         self.word2count = {}
+#         self.index2word = {0: "SOS", 1: "EOS"}
+#         self.n_words = 2  # Count SOS and EOS
 
-    def addSentence(self, sentence):
-        for word in sentence.split(' '):
-            self.addWord(word)
+#     def addSentence(self, sentence):
+#         for word in sentence.split(' '):
+#             self.addWord(word)
 
-    def addWord(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.word2count[word] = 1
-            self.index2word[self.n_words] = word
-            self.n_words += 1
-        else:
-            self.word2count[word] += 1
+#     def addWord(self, word):
+#         if word not in self.word2index:
+#             self.word2index[word] = self.n_words
+#             self.word2count[word] = 1
+#             self.index2word[self.n_words] = word
+#             self.n_words += 1
+#         else:
+#             self.word2count[word] += 1
 
-def unicodeToAscii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-    )
+# def unicodeToAscii(s):
+#     return ''.join(
+#         c for c in unicodedata.normalize('NFD', s)
+#         if unicodedata.category(c) != 'Mn'
+#     )
 
 # Lowercase, trim, and remove non-letter characters
-def normalizeString(s):
-    s = unicodeToAscii(s)   
-    return s
+# def normalizeString(s):
+#     s = unicodeToAscii(s)   
+#     return s
 
-#dataset:
-path="data.txt"
+# #dataset:
+# path="data.txt"
 
-def readLangs(lang1, lang2, reverse=True):
-    # Read the file and split into lines
-    #lines = open('Sign with Tabs.txt' % (lang1, lang2), encoding='utf-8').\
-    lines = open(path, encoding='utf-8').read().split('\n')
-    # Split every line into pairs and normalize
-    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
-    # Reverse pairs, make Lang instances
-    if reverse:
-        pairs = [list(reversed(p)) for p in pairs]
-        input_lang = Lang(lang2)
-        output_lang = Lang(lang1)
-    else:
-        input_lang = Lang(lang1)
-        output_lang = Lang(lang2)
+# def readLangs(lang1, lang2, reverse=True):
+#     # Read the file and split into lines
+#     #lines = open('Sign with Tabs.txt' % (lang1, lang2), encoding='utf-8').\
+#     lines = open(path, encoding='utf-8').read().split('\n')
+#     # Split every line into pairs and normalize
+#     pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+#     # Reverse pairs, make Lang instances
+#     if reverse:
+#         pairs = [list(reversed(p)) for p in pairs]
+#         input_lang = Lang(lang2)
+#         output_lang = Lang(lang1)
+#     else:
+#         input_lang = Lang(lang1)
+#         output_lang = Lang(lang2)
 
-    return input_lang, output_lang, pairs
-eng_prefixes = (
-    "i am ", "i m ",
-    "he is", "he s ",
-    "she is", "she s",
-    "you are", "you re ",
-    "we are", "we re ",
-    "they are", "they re "
-)
-
-
-def filterPair(p):
-    return len(p[0].split(' ')) < MAX_LENGTH and         len(p[1].split(' ')) < MAX_LENGTH and         p[1].startswith(eng_prefixes)
+#     return input_lang, output_lang, pairs
+# eng_prefixes = (
+#     "i am ", "i m ",
+#     "he is", "he s ",
+#     "she is", "she s",
+#     "you are", "you re ",
+#     "we are", "we re ",
+#     "they are", "they re "
+# )
 
 
-def filterPairs(pairs):
-    return [pair for pair in pairs if filterPair(pair)]
-def prepareData(lang1, lang2, reverse=True):
-    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
-    #pairs = filterPairs(pairs)
-    for pair in pairs:
-        input_lang.addSentence(pair[0])
-        output_lang.addSentence(pair[1])
-
-    return input_lang, output_lang, pairs
+# def filterPair(p):
+#     return len(p[0].split(' ')) < MAX_LENGTH and         len(p[1].split(' ')) < MAX_LENGTH and         p[1].startswith(eng_prefixes)
 
 
-input_lang, output_lang, pairs = prepareData('eng', 'SignLanguage', True)
+# def filterPairs(pairs):
+#     return [pair for pair in pairs if filterPair(pair)]
+# def prepareData(lang1, lang2, reverse=True):
+#     input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+#     #pairs = filterPairs(pairs)
+#     for pair in pairs:
+#         input_lang.addSentence(pair[0])
+#         output_lang.addSentence(pair[1])
 
-def indexesFromSentence(lang, sentence):
-    return [lang.word2index[word] for word in sentence.split(' ')]
+#     return input_lang, output_lang, pairs
 
 
-def tensorFromSentence(lang, sentence):
-    indexes = indexesFromSentence(lang, sentence)
-    indexes.append(EOS_token)
-    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+# input_lang, output_lang, pairs = prepareData('eng', 'SignLanguage', True)
 
-def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
-    with torch.no_grad():
-        input_tensor = tensorFromSentence(input_lang, sentence)
-        input_length = input_tensor.size()[0]
-        encoder_hidden = encoder.initHidden()
+# def indexesFromSentence(lang, sentence):
+#     return [lang.word2index[word] for word in sentence.split(' ')]
 
-        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
 
-        for ei in range(input_length):
-            encoder_output, encoder_hidden = encoder(input_tensor[ei],
-                                                     encoder_hidden)
-            encoder_outputs[ei] += encoder_output[0, 0]
+# def tensorFromSentence(lang, sentence):
+#     indexes = indexesFromSentence(lang, sentence)
+#     indexes.append(EOS_token)
+#     return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
 
-        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
+# def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+#     with torch.no_grad():
+#         input_tensor = tensorFromSentence(input_lang, sentence)
+#         input_length = input_tensor.size()[0]
+#         encoder_hidden = encoder.initHidden()
 
-        decoder_hidden = encoder_hidden
+#         encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
 
-        decoded_words = []
-        decoder_attentions = torch.zeros(max_length, max_length)
+#         for ei in range(input_length):
+#             encoder_output, encoder_hidden = encoder(input_tensor[ei],
+#                                                      encoder_hidden)
+#             encoder_outputs[ei] += encoder_output[0, 0]
 
-        for di in range(max_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
-                decoder_input, decoder_hidden, encoder_outputs)
-            decoder_attentions[di] = decoder_attention.data
-            topv, topi = decoder_output.data.topk(1)
-            if topi.item() == EOS_token:
-                decoded_words.append('<EOS>')
-                break
-            else:
-                decoded_words.append(output_lang.index2word[topi.item()])
+#         decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
 
-            decoder_input = topi.squeeze().detach()
+#         decoder_hidden = encoder_hidden
 
-        return decoded_words, decoder_attentions[:di + 1]
+#         decoded_words = []
+#         decoder_attentions = torch.zeros(max_length, max_length)
+
+#         for di in range(max_length):
+#             decoder_output, decoder_hidden, decoder_attention = decoder(
+#                 decoder_input, decoder_hidden, encoder_outputs)
+#             decoder_attentions[di] = decoder_attention.data
+#             topv, topi = decoder_output.data.topk(1)
+#             if topi.item() == EOS_token:
+#                 decoded_words.append('<EOS>')
+#                 break
+#             else:
+#                 decoded_words.append(output_lang.index2word[topi.item()])
+
+#             decoder_input = topi.squeeze().detach()
+
+#         return decoded_words, decoder_attentions[:di + 1]
 
 def evaluateASA(input_sentence):
     output_words, attentions = evaluate(encoder, decoder, input_sentence)
