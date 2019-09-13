3c01728eea691e670598252f666178b6f5af18c3
Ahmad Zia <ah.ziayosfi@gmail.com>
2019-09-13 23:15:15 +0430

    Update trainig.py

--- trainig.py
+++ trainig.py
@@ -40,7 +40,7 @@ import random
 import torch
 import torch.nn as nn
 from torch import optim
-
+import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
