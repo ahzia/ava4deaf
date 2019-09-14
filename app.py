080df8b6e1705398304361676e9507352e899031
Ahmad Zia <ah.ziayosfi@gmail.com>
2019-09-14 07:01:22 +0430

    Update app.py

--- app.py
+++ app.py
@@ -224,7 +224,8 @@ def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
     with torch.no_grad():
         input_tensor = tensorFromSentence(input_lang, sentence)
         input_length = input_tensor.size()[0]
-        encoder_hidden =getattr(encoder,initHidden())
+        encoder_hidden=EncoderRNN.initHidden(encoder)
+        #encoder_hidden = encoder.initHidden()
         encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
 
         for ei in range(input_length):
