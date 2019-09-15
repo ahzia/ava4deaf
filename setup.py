e9a78df03c8ab6c550a724e8ff39611c65fc5cd5
Ahmad Zia <ah.ziayosfi@gmail.com>
2019-09-15 15:35:52 +0430

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,303 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# get_ipython().run_line_magic('matplotlib', 'inline')
+# import pickle
+
+
+# 
+# Translation with a Sequence to Sequence Network and Attention
+# *************************************************************
+# the orignal author of this notebook:
+# **Author**: `Sean Robertson <https://github.com/spro/practical-pytorch>`_
+# **Modified by**:Ahmad Zia Yousufi
+# **For**:Global pytorch summer hackathon
+# **Project**:AVA - toward a Machine Translation Platform for Deaf 
+# we used the orignal notebook to implement a new idea,the idea is to translate English text to sign language using a neural network.
+# ... to varying degrees of success.
+# 
+# This is made possible by the simple but powerful idea of the `sequence
+# to sequence network <http://arxiv.org/abs/1409.3215>`__, in which two
+# recurrent neural networks work together to transform one sequence to
+# another. An encoder network condenses an input sequence into a vector,
+# and a decoder network unfolds that vector into a new sequence.
+# 
+# To improve upon this model we'll use an `attention mechanism <https://arxiv.org/abs/1409.0473>`__, which lets the decoder
+# learn to focus over a specific range of the input sequence.
+# 
+# **Requirements**
+
+# In[31]:
+
+
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+
+
+# Loading data files
+# ==================
+# 
+# The data for this project is a set of English to Sign Writing translation pairs.
+# we gathered thes pairs from defrent sources like Sign Writing Wikipedia,SignPuddle website and some sign writing Books.
+
+# Similar to the character encoding used in the character-level RNN
+# tutorials, we will be representing each word in a language as a one-hot
+# vector, or giant vector of zeros except for a single one (at the index
+# of the word). 
+
+# We'll need a unique index per word to use as the inputs and targets of
+# the networks later. To keep track of all this we will use a helper class
+# called ``Lang`` which has word → index (``word2index``) and index → word
+# (``index2word``) dictionaries, as well as a count of each word
+# ``word2count`` to use to later replace rare words.
+# 
+# 
+# 
+
+# In[32]:
+
+
+
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+
+# In[33]:
+
+
+#the data set is stored in a text file data.txt
+
+# The files are all in Unicode, to simplify we will turn Unicode
+# characters to ASCII, make everything lowercase, and trim most
+# punctuation.
+# 
+# 
+# 
+
+# In[34]:
+
+
+# Turn a Unicode string to plain ASCII, thanks to
+# http://stackoverflow.com/a/518232/2809427
+
+
+
+# To read the data file we will split the file into lines, and then split
+# lines into pairs. The files are all English → Other Language, so if we
+# want to translate from Other Language → English I added the ``reverse``
+# flag to reverse the pairs.
+# 
+# 
+# 
+
+# In[35]:
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+# The Decoder
+# -----------
+# 
+# The decoder is another RNN that takes the encoder output vector(s) and
+# outputs a sequence of words to create the translation.
+# 
+# 
+# 
+
+# Simple Decoder
+# ^^^^^^^^^^^^^^
+# 
+# In the simplest seq2seq decoder we use only last output of the encoder.
+# This last output is sometimes called the *context vector* as it encodes
+# context from the entire sequence. This context vector is used as the
+# initial hidden state of the decoder.
+# 
+# At every step of decoding, the decoder is given an input token and
+# hidden state. The initial input token is the start-of-string ``<SOS>``
+# token, and the first hidden state is the context vector (the encoder's
+# last hidden state).
+
+# In[39]:
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+# to save space we'll be going straight for the gold and introducing the
+# Attention Mechanism.
+
+# Attention Decoder
+# ^^^^^^^^^^^^^^^^^
+# 
+# If only the context vector is passed betweeen the encoder and decoder,
+# that single vector carries the burden of encoding the entire sentence.
+# 
+# Attention allows the decoder network to "focus" on a different part of
+# the encoder's outputs for every step of the decoder's own outputs. First
+# we calculate a set of *attention weights*. These will be multiplied by
+# the encoder output vectors to create a weighted combination. The result
+# (called ``attn_applied`` in the code) should contain information about
+# that specific part of the input sequence, and thus help the decoder
+# choose the right output words.
+# Calculating the attention weights is done with another feed-forward
+# layer ``attn``, using the decoder's input and hidden state as inputs.
+# Because there are sentences of all sizes in the training data, to
+# actually create and train this layer we have to choose a maximum
+# sentence length (input length, for encoder outputs) that it can apply
+# to. Sentences of the maximum length will use all the attention weights,
+# while shorter sentences will only use the first few.
+
+# In[40]:
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+# <div class="alert alert-info"><h4>Note</h4><p>There are other forms of attention that work around the length
+#   limitation by using a relative position approach. Read about "local
+#   attention" in `Effective Approaches to Attention-based Neural Machine
+#   Translation <https://arxiv.org/abs/1508.04025>`__.</p></div>
+# 
+# Training
+# ========
+# 
+# Preparing Training Data
+# -----------------------
+# 
+# To train, for each pair we will need an input tensor (indexes of the
+# words in the input sentence) and target tensor (indexes of the words in
+# the target sentence). While creating these vectors we will append the
+# EOS token to both sequences.
+# 
+# 
+# 
+
+# In[41]:
+
+
+
+# Training the Model
+# ------------------
+# 
+# To train we run the input sentence through the encoder, and keep track
+# of every output and the latest hidden state. Then the decoder is given
+# the ``<SOS>`` token as its first input, and the last hidden state of the
+# encoder as its first hidden state.
+# 
+# "Teacher forcing" is the concept of using the real target outputs as
+# each next input, instead of using the decoder's guess as the next input.
+# Using teacher forcing causes it to converge faster but `when the trained
+# network is exploited, it may exhibit
+# instability <http://minds.jacobs-university.de/sites/default/files/uploads/papers/ESNTutorialRev.pdf>`__.
+# 
+# You can observe outputs of teacher-forced networks that read with
+# coherent grammar but wander far from the correct translation -
+# intuitively it has learned to represent the output grammar and can "pick
+# up" the meaning once the teacher tells it the first few words, but it
+# has not properly learned how to create the sentence from the translation
+# in the first place.
+# 
+# Because of the freedom PyTorch's autograd gives us, we can randomly
+# choose to use teacher forcing or not with a simple if statement. Turn
+# ``teacher_forcing_ratio`` up to use more of it.
+# 
+# 
+# 
+
+# In[42]:
